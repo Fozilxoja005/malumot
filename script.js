ada6241a64d28a738aba0b66808a20215e6b89ed
Fozilxoja <fozilxoja@gmail.com>
2022-04-09 14:30:40 +0500

    malumot

---
+++ script.js
@@ -0,0 +1,70 @@
+let son ;
+for (let i = 0; i < 10; i++) {
+    let ism = prompt("ismingizni kiriting ")
+    let yosh = +prompt("yoshingizni kiriting ")
+    let ish = prompt("ish joyingizni kiriting ")
+    console.log('sizning ismingiz' + ism);
+    console.log('sizning yoshingiz' + yosh);
+    console.log('ish joyingiz' + ish);
+} 
+
+
+// let Foydalanuvchilar = {
+//   foydalanuvchilar1:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar1:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar2:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar3:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar4:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar5:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar6:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar7:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar8:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar9:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   },
+//   foydalanuvchilar10:{
+//       ismi: prompt('ismingizni kiriting '),
+//       yosh: prompt('yoshingizni kiriting '),
+//       ishi__joyi: prompt('ish joyingizni kiriting ')
+//   }
+// }
+// console.log(Foydalanuvchilar);
+
